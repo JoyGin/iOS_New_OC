d251e459f15fed162aef65e918f8673e05e708f9
george <1063658094@qq.com>
2019-05-23 16:11:12 +0800

    Initial Commit

---
+++ MyTTNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTTNews
+//
+//  Created by george on 2019/5/23.
+//  Copyright © 2019 com.george. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
